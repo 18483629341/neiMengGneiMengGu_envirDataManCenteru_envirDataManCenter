3befed89dc4728d62de4a6f2382ecdb984b80a04
lisa091500 <714402934@qq.com>
2019-03-15 17:11:31 +0800

    ond

--- js/circle_translate.js
+++ js/circle_translate.js
@@ -12,20 +12,29 @@ function Circle(element, angle) {
 	$(element).hover(function(){
 		clearInterval(turnOn);
 	},function(){
-		turnOn=setInterval(function(){
+		turnOn=
+		setInterval(function(){
 			//顺时针旋转 ++，逆时针旋转  --
 			_this.turn();
-		}, 100)
+		},1)
+
 	}).trigger('mouseleave');
 	//循环一次执行的方法
 	this.turn=function(){
-		this.angle=this.angle+3;
+		//window.requestAnimFrame(this.turn)
+		this.angle=this.angle+0.5;
 		//0.017453293为角度转为弧度的比率值，1度=0.017453293弧度；Math.cos(x),x为弧度
-		$(element).css({
+		$(element).animate({
 			'left':this.rx*Math.cos(this.angle*0.017453293)+this.rx-this.sideLength/2+'px',
 			'top':this.ry*Math.sin(this.angle*0.017453293)+this.ry-this.sideLength/2+'px',
 		})
 	}
 }
-// var circleItem1 = new Circle($('#rotateItem1'), 180);
-// var circleItem2 = new Circle($('#rotateItem2'), 90);
+window.requestAnimFrame = (function() {
+	return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
+		function( /* function FrameRequestCallback */ callback, /* DOMElement Element */ element) {
+			return window.setTimeout(callback, 1000 / 60);
+		};
+})();
+var circleItem1 = new Circle($('#rotateItem1'), 180);
+var circleItem2 = new Circle($('#rotateItem2'), 90);
