a974b18afc4e8830cc071edd4a3ae7eed3c52d25
18483629341 <714402934@qq.com>
2019-03-22 10:13:16 +0800

    2019.3.22终极版本1

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ $(function () {
     var lightLoopLeft = new LightLoop('#LightBox', 10, "converage");//converage 往集中方向
     var lightLoopRight = new LightLoop('#LightBoxRight', 8, 'converage');
 
-    //数据汇聚和数据服务的循环切换  使用setInterval()使用与内部的animate()时间有误差，不能在准确的时间，间隔
+    //数据汇聚和数据服务的循环切换  使用setInterval()使用与内部的animate()时间有误差，做出的效果不稳定；不能在准确的时间，间隔
     // LeftRightLoop();
     // var x=0;
     // function LeftRightLoop(){
